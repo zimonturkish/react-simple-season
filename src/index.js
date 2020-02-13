426719dae6ef07a12936163e746afab30d368d9b
Simone Turco <simone.turco@charite.de>
2020-02-13 17:06:15 +0100

    alternative way to initialize state

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, errorMessage: "" };
-  }
+  state = { let: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
