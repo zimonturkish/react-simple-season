5ea45c05167cf48c5bbe3c89003e21dbc3899480
Simone Turco <simone.turco@charite.de>
2020-02-13 16:50:33 +0100

    geolocation func in the componentDidMount

--- src/index.js
+++ src/index.js
@@ -5,22 +5,16 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
+      position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
     );
   }
 
-  componentDidMount() {
-    console.log("My component was rendered to the screen");
-  }
-  componentDidUpdate() {
-    console.log("My component was just updated - it rerendered");
-  }
   // React says we have to define render!!
   render() {
     if (this.state.errorMessage && !this.state.lat) {
