38edcf359d7a20205aeee8861dff2c1acc03158a
Simone Turco <simone.turco@charite.de>
2020-02-13 15:07:49 +0100

    this.state for error about geolocation

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => this.setState({ errorMessage: err.message })
     );
-    return <div>Latitude: </div>;
+  }
+  // React says we have to define render!!
+  render() {
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
