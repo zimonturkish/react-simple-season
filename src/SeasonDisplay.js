332722fbce421a6889f9ba0f426bfe60d394071f
Simone Turco <simone.turco@charite.de>
2020-02-14 11:08:04 +0100

    adding an icon: first step

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -12,10 +12,13 @@ const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
   const text =
     season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+  const icon = season === "winter" ? "snowflake" : "sun";
 
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
