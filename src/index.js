433ca93b133e627873b3fd10d319807dedb3a360
Simone Turco <simone.turco@charite.de>
2020-02-14 14:17:05 +0100

    example to avoid conditional rendering

--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ class App extends React.Component {
     );
   }
 
-  // React says we have to define render!!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -24,6 +23,11 @@ class App extends React.Component {
     }
 
     return <Spinner message="Please accept location request" />;
+  }
+
+  // React says we have to define render!!
+  render() {
+    return <div className="boreder red">{this.renderContent()}</div>;
   }
 }
 
