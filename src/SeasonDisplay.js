02da08e49b0ea9ecaaad3fbed12759de517ca438
Simone Turco <simone.turco@charite.de>
2020-02-14 10:54:45 +0100

    season => text message

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,9 +10,14 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
 
-  console.log(season);
-  return <div>Season Display</div>;
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
