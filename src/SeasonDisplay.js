ba62d528d32d2db34721a0e4076a0dc75ccf6215
Simone Turco <simone.turco@charite.de>
2020-02-14 10:46:53 +0100

    function to determine season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,17 @@
 import React from "react";
 
-const SeasonDisplay = props => {
-  console.log(props.lat);
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
 
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  console.log(season);
   return <div>Season Display</div>;
 };
 
