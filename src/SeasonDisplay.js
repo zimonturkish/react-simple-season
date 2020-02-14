b4e744f54a3163297524a94d222f08d61d3be293
Simone Turco <simone.turco@charite.de>
2020-02-14 14:24:06 +0100

    replacing SeasonDispay component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./SeasonDisplay.css";
 
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
+};
+
 const seasonConfig = {
   summer: {
     text: "Let's hit the beach!",
@@ -18,19 +31,6 @@ const getSeason = (lat, month) => {
   } else {
     return lat > 0 ? "winter" : "summer";
   }
-};
-
-const SeasonDisplay = props => {
-  const season = getSeason(props.lat, new Date().getMonth());
-  const { text, iconName } = seasonConfig[season];
-
-  return (
-    <div className={`season-display ${season}`}>
-      <i className={`icon-left massive ${iconName} icon`} />
-      <h1>{text}</h1>
-      <i className={`icon-right massive ${iconName} icon`} />
-    </div>
-  );
 };
 
 export default SeasonDisplay;
