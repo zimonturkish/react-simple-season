644ce7e0e7a59fa2444e024c48b7f77d6b78514b
Turco <turcos@charite.de>
2020-02-17 10:56:14 +0100

    Updates

---
+++ precache-manifest.7924819489b19eb280f8b381b31ace6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff126480bd5ed43aa111d20c837446b",
+    "url": "/react-simple-season/index.html"
+  },
+  {
+    "revision": "d078df024b04ae4ffbf9",
+    "url": "/react-simple-season/static/css/main.2f2246de.chunk.css"
+  },
+  {
+    "revision": "2efb493eb23cdeb3978f",
+    "url": "/react-simple-season/static/js/2.8d0e5624.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-simple-season/static/js/2.8d0e5624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d078df024b04ae4ffbf9",
+    "url": "/react-simple-season/static/js/main.2804a68b.chunk.js"
+  },
+  {
+    "revision": "bab40c3219e0cfcd069b",
+    "url": "/react-simple-season/static/js/runtime-main.2593891e.js"
+  }
+]);
